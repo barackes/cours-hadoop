b503f1205e79946f9d35c90c5e21a117e550d55f
Slim Tebourbi <slim@tebourbi.com>
2018-03-19 14:30:36 +0100

    add sample job+ test

--- source-code/spark-sample-project/src/main/scala/example/NoWordCountProcessor.scala
+++ source-code/spark-sample-project/src/main/scala/example/NoWordCountProcessor.scala
@@ -1,10 +1,13 @@
 package example
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class NoWordCountProcessor(val sparkSession: SparkSession) {
 
-  def run() : Unit = {}
+  import org.apache.spark.sql.functions._
+  def countStudents(studentsDF:DataFrame)  = {
+    studentsDF.agg(countDistinct("student").as("number_of_students"))
+  }
 
 }
 
--- source-code/spark-sample-project/src/test/scala/example/NoWordCountProcessorSpec.scala
+++ source-code/spark-sample-project/src/test/scala/example/NoWordCountProcessorSpec.scala
@@ -6,18 +6,34 @@ import org.scalatest._
 
 class NoWordCountProcessorSpec extends FlatSpec with Matchers {
 
-  it must "simply run" in {
 
-    val sparkConf = new SparkConf()
-                      .setMaster("local[1]")
-                        .setAppName("no-word-count-test")
+  val sparkConf = new SparkConf()
+    .setMaster("local[1]")
+    .setAppName("no-word-count-test")
 
-    val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+  val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
 
-    new NoWordCountProcessor(sparkSession).run()
+  it must "count num of students" in {
+
+    import sparkSession.implicits._
+
+    val students = Seq(
+      StudentNote("e1","c1",10.0d)
+      , StudentNote("e2","c2",10.0d)
+      , StudentNote("e1","c1",10.0d)
+      , StudentNote("e2","c2",10.0d)
+    )
+
+
+    val studentsDF = students.toDF()
+
+    val result = new NoWordCountProcessor(sparkSession).countStudents(studentsDF)
+    assert(2l === result.collect()(0).getLong(0))
 
 
   }
 
 
 }
+
+case class StudentNote(student:String,course:String,note:Double)
